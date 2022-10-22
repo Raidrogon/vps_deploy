69b21eb22476483ffe5cd761574d994de843ea7a
crab <imrcrab@163.com>
2022-10-23 00:49:53 +0800

    Update vnstat.sh

--- xray/vnstat.sh
+++ xray/vnstat.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-ipnow=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
-echo ${ipnow}
+
 vnstati -s -o /tmp/summary.png
 
 curl -4 -F "chat_id=639130306" \
-     -F "caption=\"`hostname`
-${ipnow} \"" \
+     -F "caption=\"`hostname`\"" \
      -F "photo=@/tmp/summary.png" \
      https://api.telegram.org/bot1896867188:AAGD9umD76RWDLpCInqFBfQbQU9BBRlWhZ8/sendPhoto
 
