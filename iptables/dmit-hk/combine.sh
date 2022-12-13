c3a449bce019b62b35aba5036dbfa5e092216b24
crab <imrcrab@163.com>
2022-12-13 11:27:16 +0800

    remove: black

--- iptables/dmit-hk/combine.sh
+++ iptables/dmit-hk/combine.sh
@@ -9,28 +9,11 @@ if [[ -f "${w_ip_path}" ]]; then
       # cat ${w_ip_path} | while read line
       while read line || [[ -n ${line} ]]
       do
-            for ele in {1443..1500}
-            do 
-                  if [[ "${line}" =~ ^\#.* ]] || [[ ! -n ${line} ]];then
-                        continue
-                  fi
-                  echo "-A INPUT -s ${line} -p tcp -m tcp --dport ${ele} -j ACCEPT" >> iptables.conf
-            done
+        if [[ "${line}" =~ ^\#.* ]] || [[ ! -n ${line} ]];then
+            continue
+        fi
+        echo "-A INPUT -s ${line} -p tcp -m tcp --dport 1443:1500 -j ACCEPT" >> iptables.conf
       done < ${w_ip_path}
-fi
-
-
-if [[ -f "${b_ip_path}" ]]; then
-      while read line || [[ -n ${line} ]]
-      do
-            for ele in {1443..1500}
-            do 
-                  if [[ "${line}" =~ ^\#.* ]] || [[ ! -n ${line} ]];then
-                        continue
-                  fi
-                  echo "-A INPUT -s ${line} -p tcp -m tcp --dport ${ele} -j DROP" >> iptables.conf
-            done
-      done < ${b_ip_path}
 fi
 
 cat end.conf >> iptables.conf
