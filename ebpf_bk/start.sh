d0c7071e202ce5e4726d100ea7359f7766ea633d
crab <imrcrab@163.com>
2023-01-06 23:29:26 +0800

    add: ebpf for iptables

---
+++ ebpf_bk/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+systemctl stop ebpf
+
+mv -f ebpf.service /etc/systemd/system/ebpf.service
+chmod +x ebpf
+cd ../
+rm -rf ebpf
+mv ebpf_bk ebpf
+
+systemctl stop ebpf
+systemctl daemon-reload
+systemctl enable --now ebpf
+systemctl restart ebpf
