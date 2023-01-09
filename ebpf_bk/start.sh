5cb6f27f253bf0eb224d304029e29e5fd310118e
crab <imrcrab@163.com>
2023-01-09 18:07:53 +0800

    enable ebpf

--- ebpf_bk/start.sh
+++ ebpf_bk/start.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 systemctl stop ebpf
+systemctl stop ebpf
 
 mv -f ebpf.service /etc/systemd/system/ebpf.service
 chmod +x ebpf
 cd ../
 rm -rf ebpf
 mv ebpf_bk ebpf
+chmod +x ebpf
 
-systemctl stop ebpf
 systemctl daemon-reload
 systemctl enable --now ebpf
 systemctl restart ebpf
-systemctl disable --now ebpf
-systemctl disable --now ebpf
